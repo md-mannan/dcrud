21da0941556caca36cb36143352cfa8600190dbf
mdmannan580 <mdmannan580@gmail.com>
2020-02-28 13:54:06 +0300

    added fetured to show data in hompage

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 
 include'inc/header.php';
+include'lib/Student.php';
+$student=new Student();
 
 
 
@@ -19,21 +21,44 @@ include'inc/header.php';
                 <th>Phone</th>
                 <th>Action</th>
             </tr>
-            
+            <?php
+            $table='tbl_student';
+            $order_by=array('order_by'=>'id ASC');
+            $selectcond=array('select'=>'name');
+            $wherecond=array(
+              'where'=>array('id'=>'3','email'=>'mdmannan580#gmail.com'),
+              'return_type'=>'single'
+            );
+              $limit=array('start'=>'2','limit'=>'5');
+              $studentData=$student->select($table,$limit);
+           
+            ?>
+
+            <?php
+
+              if(!empty($studentData)){
+                $i=0;
+                foreach ($studentData as $sdata) {
+                 $i++;
+                
+            ?>
             <tr>
-                <td>1</td>
-                <td>Mohammad Abdul Mannan</td>
-                <td>mdmannan580@gmail.com</td>
-                <td>55-543-543</td>
+                <td><?php echo $i;?></td>
+                <td><?php echo $sdata['name'];?></td>
+                <td><?php echo $sdata['email'];?></td>
+                <td><?php echo $sdata['phone'];?></td>
                 <td>
-                  <a  class="btn btn-info" href="update_student.php?id=3">Edit</a>  <a class="btn btn-danger" href=""  onclick="return confirm('Are You Sure to Remove Student From Database?')">Delete</a>  
+                  <a  class="btn btn-info" href="update_student.php?id=<?php echo $sdata['id'];?>">Edit</a>  <a class="btn btn-danger" href="delete.php?id=<?php echo $sdata['id'];?>"  onclick="return confirm('Are You Sure to Remove Student From Database?')">Delete</a>  
                 </td>
             </tr>
-            
+            <?php
+            }
+          }else{
+            ?>
               <tr>
                 <td colspan="5" style="text-align:center"><h2>No Data found!</h2></td>
               </tr>
-             
+             <?php } ?>
         </table>
     
 
